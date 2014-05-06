f3152f64b3ffee4bb9faf86ec2bb56ddde94f515
Ian Moffitt <nessthehero@gmail.com>
2014-05-06 14:56:22 -0500

    Expanded on README

--- out/lunr-index.js
+++ out/lunr-index.js
@@ -114,11 +114,25 @@ index.add({
     body: "# README  "
 });
 
+documentTitles["readme.html#front-end-coding-standards"] = "Front-end Coding Standards";
+index.add({
+    url: "readme.html#front-end-coding-standards",
+    title: "Front-end Coding Standards",
+    body: "## Front-end Coding Standards  The standards by which the dev team at BarkleyREI in Pittsburgh codes every project.  Uses [beautifuldocs](http://beautifuldocs.com)  "
+});
+
 documentTitles["readme.html#how-to-compile"] = "How to compile";
 index.add({
     url: "readme.html#how-to-compile",
     title: "How to compile",
-    body: "## How to compile  `npm install -g beautifuldocs`  Navigate to directory in terminal  `bfdocs manifest.json` "
+    body: "## How to compile  `npm install -g beautifuldocs`  Navigate to directory in terminal  `bfdocs manifest.json`  "
+});
+
+documentTitles["readme.html#how-to-push-to-gh-pages"] = "How to push to gh-pages";
+index.add({
+    url: "readme.html#how-to-push-to-gh-pages",
+    title: "How to push to gh-pages",
+    body: "## How to push to gh-pages  `git add .`  `git commit -m \&quot;blah blah commit message\&quot;`  `git push origin master`  At this point you have pushed it to the github repository (hopefully your forked version)  Now to subtree into the pages branch  `git subtree push --prefix out origin gh-pages`  No futher steps, this pushes it to the branch and up to the repo "
 });
 
 
